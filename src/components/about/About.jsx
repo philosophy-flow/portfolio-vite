5a2117a145ce8527f6a35f544cb96c6c1658a75a
Jacob Stewart <jacob.g.stewart@icloud.com>
2025-01-04 17:26:31 -0500

    asset update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -39,7 +39,7 @@ export default function About() {
             </Link>
             <a
               className="about-nav-link"
-              href="https://pdfhost.io/v/3klOdtKhy_jacob_stewart_resume"
+              href="https://pdfhost.io/v/AW5hOWTkG_jacob_stewart_resume"
               target="_blank"
               rel="noreferrer"
             >
