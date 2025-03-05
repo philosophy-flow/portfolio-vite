76bc38de7d5312cd8301689eb53e0bc944fd5940
Jacob Stewart <jacob.g.stewart@icloud.com>
2025-03-05 16:16:24 -0500

    add typewriter to homepage

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,23 +1,43 @@
 import "./Home.css";
 
 import { motion } from "framer-motion";
+import Typewriter from "typewriter-effect";
 
 export default function Home() {
-  return (
-    <>
-      <motion.section
-        className="Home"
-        initial={{ opacity: 0, y: 25 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 25 }}
-        transition={{ duration: 0.5 }}
-      >
-        <div className="home-content-container">
-          <h1 className="home-text">
-            Hi. I'm Jacob, an NYC based software developer.
-          </h1>
-        </div>
-      </motion.section>
-    </>
-  );
+    return (
+        <>
+            <motion.section
+                className="Home"
+                initial={{ opacity: 0, y: 25 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 25 }}
+                transition={{ duration: 0.5 }}
+            >
+                <div className="home-content-container">
+                    <h1 className="home-text">
+                        <span style={{ fontSize: "inherit" }}>Hi. I'm</span>{" "}
+                        <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter
+                                    .typeString("Jacob,")
+                                    .callFunction((typewriter) => {
+                                        setTimeout(() => {
+                                            typewriter.elements.cursor.remove(),
+                                                typewriter.elements.wrapper.classList.add(
+                                                    "underlined"
+                                                );
+                                        }, 500);
+                                    })
+                                    .start();
+                            }}
+                        />
+                        <span style={{ fontSize: "inherit" }}>
+                            {" "}
+                            an NYC based software developer.
+                        </span>
+                    </h1>
+                </div>
+            </motion.section>
+        </>
+    );
 }
