e88973ddf05cd6f37d73b6e6230f5a69ec504f31
Jacob Stewart <jacob.g.stewart@icloud.com>
2025-03-08 13:16:00 -0500

    refactor to use svg for icon instead of external

--- src/components/projects/ScrollButton.jsx
+++ src/components/projects/ScrollButton.jsx
@@ -1,33 +1,34 @@
 import { useState } from "react";
+import UpArrow from "../icons/UpArrow";
 
 export default function ScrollButton() {
-  const [visible, setVisible] = useState(false);
+    const [visible, setVisible] = useState(false);
 
-  const toggleVisible = () => {
-    const scrolled = document.documentElement.scrollTop;
-    if (scrolled > 300) {
-      setVisible(true);
-    } else if (scrolled <= 300) {
-      setVisible(false);
-    }
-  };
+    const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 300) {
+            setVisible(true);
+        } else if (scrolled <= 300) {
+            setVisible(false);
+        }
+    };
 
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
 
-  window.addEventListener("scroll", toggleVisible);
+    window.addEventListener("scroll", toggleVisible);
 
-  return (
-    <button
-      className="scroll-button"
-      style={{ display: visible ? "inline" : "none" }}
-      onClick={scrollToTop}
-    >
-      <i className="fas fa-arrow-up fa-2x"></i>
-    </button>
-  );
+    return (
+        <button
+            className="scroll-button"
+            style={{ display: visible ? "inline" : "none" }}
+            onClick={scrollToTop}
+        >
+            <UpArrow />
+        </button>
+    );
 }
